c7c406362382c9dbc1f107da91d3e943f75b6268
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-06 21:04:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 const fetchEvents = require('./services/fetchEvents');
-const pingServer = require('./services/pingServer');
+// const pingServer = require('./services/pingServer');
 
 const { DB_HOST, PORT } = process.env;
 
@@ -16,8 +16,8 @@ mongoose
     setInterval(fetchEvents, 6 * 60 * 60 * 1000);
     fetchEvents();
 
-    setInterval(pingServer, 19 * 60 * 1000);
-    pingServer();
+    // setInterval(pingServer, 19 * 60 * 1000);
+    // pingServer();
   })
   .catch(error => {
     console.log(error.message);
